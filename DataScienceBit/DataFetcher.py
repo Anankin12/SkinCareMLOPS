b2f34fd540755d93ac4fb13f3f56d942e4c73e7c
Anankin12 <alberto.russo@studenti.unitn.it>
2025-01-28 18:40:54 +0100

    Changed data fetcher

--- DataScienceBit/DataFetcher.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("sephora-products-and-skincare-reviews")
-
-print("Path to dataset files:", path)
---
+++ DataScienceBit/DataFetcher.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o ./sephora-products-and-skincare-reviews.zip\
+  https://www.kaggle.com/api/v1/datasets/download/nadyinky/sephora-products-and-skincare-reviews
